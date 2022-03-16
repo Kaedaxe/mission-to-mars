822e972011ff3480b820ca51bd34711efbc666b5
Dave <davidkaye118@gmail.com>
2022-03-15 22:52:46 -0400

    Module 10 Challenge

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
-from bs4 import BeautifulSoup as soup
 
 app = Flask(__name__)
+app.run(debug=True)
 
 # Use flask_pymongo to set up a mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
--- scraping.py
+++ scraping.py
@@ -18,7 +18,8 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemisphere_images": hemisphere_images(browser)
     }
 
     browser.quit()
@@ -92,6 +93,50 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def hemisphere_images(browser):
+    url = 'https://marshemispheres.com/'
+    # 1. Use browser to visit the URL
+    browser.visit(url)
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for x in range(4):
+        # Create empty dictionary for title, URL pairs
+        hemispheres_x = {}
+        # Creating path to each hemisphere's page and clicking the link to navigate to each page
+        path = browser.find_by_tag('h3')[x].click()
+        # Initializing splinter and URL references
+        url = 'https://marshemispheres.com/'
+        html = browser.html
+        # Using Beautiful Soup to parse the resulting hemisphere pages' HTML
+        image_soup = soup(html, 'html.parser')
+        # Searching Soup HTML to find container for title and URL sections
+        hemis = image_soup.find_all('div', class_="container")
+        
+        # Creating Try-Except section for collecting title and URL data and entering those into dictionary and list of each dict.
+        try:
+            for hemisphere in hemis:           
+                image_path = hemisphere.select('li > a', limit=1)
+                # For loop that removes the url information from the href section in the tag
+                for link in image_path:
+                    image = link['href']
+                # Creating full URL for each image
+                img_url = f'{url}{image}'
+                # Grabbing each title relating to each hemisphere
+                title = hemisphere.find('h2').get_text()
+                hemispheres_x['img_url'] = img_url
+                hemispheres_x['title'] = title
+        except Exception as e:
+            print(e)
+        # Adding hemisphere dictionaries to list
+        hemisphere_image_urls.append(hemispheres_x)
+        # Browser returns to initial mars page for next iteration
+        browser.back()
+
+    return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
